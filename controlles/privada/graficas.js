b26c78f0998e71444e40376b3e6b569b30949b33
Ricardo Nicolas Melara <ricardo06melara@gmail.com>
2024-04-22 08:11:04 -0600

    pagina principal y otras cosas

---
+++ controlles/privada/graficas.js
@@ -0,0 +1,32 @@
+var gx1 = document.getElementById("grafica_1").getContext("2d")
+var myChart = new Chart(gx1, {
+    type: "bar",
+    data: {
+        labels: ['espalda', 'huesos', 'columna'],
+        datasets: [{
+            labels: 'Total ventas',
+            data: [68, 97, 30],
+        }]
+    },
+    Options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
+
+var gx2 = document.getElementById("grafica_2").getContext("2d")
+var myChart2 = new Chart(gx2, {
+    type: "pie",
+    data: {
+        labels: ['espalda', 'huesos', 'columna'],
+        datasets: [{
+            labels: 'Total ventas',
+            data: [68, 97, 30],
+        }]
+    }
+})
