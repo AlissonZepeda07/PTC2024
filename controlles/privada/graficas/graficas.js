d89e64356afa8ed499c9c0a16adc6f7ee16d2f42
Ricardo Nicolas Melara <ricardo06melara@gmail.com>
2024-04-23 14:21:17 -0600

    actualizacion de footer en pag principal

--- controlles/privada/graficas/graficas.js
+++ controlles/privada/graficas/graficas.js
@@ -2,7 +2,7 @@ var gx1 = document.getElementById("grafica_1").getContext("2d")
 var myChart = new Chart(gx1, {
     type: "bar",
     data: {
-        labels: ['espalda', 'huesos', 'columna'],
+        labels: ['Masajes', 'Paquete Primera vez','Acupuntura'],
         datasets: [{
             labels: 'Total ventas',
             data: [68, 97, 30],
@@ -23,7 +23,7 @@ var gx2 = document.getElementById("grafica_2").getContext("2d")
 var myChart2 = new Chart(gx2, {
     type: "pie",
     data: {
-        labels: ['espalda', 'huesos', 'columna'],
+        labels: ['Masajes', 'Paquete Primera vez', 'Acupuntura'],
         datasets: [{
             labels: 'Total ventas',
             data: [68, 97, 30],
