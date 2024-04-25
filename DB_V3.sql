5bdc9495ae7dcd1ee39d6760e8334b8e2c5e8bc4
Ricardo Nicolas Melara <107507393+ElRicki0@users.noreply.github.com>
2024-04-24 20:20:13 -0600

    Update DB_V3.sql
    actualizacion con ayuda de profe Carranza

--- DB_V3.sql
+++ DB_V3.sql
@@ -8,10 +8,11 @@ CREATE TABLE tb_clientes(
 id_cliente int primary key auto_increment not null,
 nombre_cliente VARCHAR (50) NOT NULL,
 apellido_cliente VARCHAR (50) NOT NULL,
-dui_cliente VARCHAR (10) NOT NULL, 
+dui_cliente VARCHAR (10), 
 correo_cliente VARCHAR (100) NOT NULL,
 telefono_cliente VARCHAR (9) NOT NULL, 
-nacimiento_cliente DATE NOT NULL
+nacimiento_cliente DATE NOT NULL,
+responsable_cliente varchar(50)
 ); 
  
  CREATE TABLE tb_testimonios(
@@ -22,14 +23,15 @@ nacimiento_cliente DATE NOT NULL
 FOREIGN KEY (id_cliente) REFERENCES tb_clientes (id_cliente)
  );
  
-CREATE TABLE tb_admin(
-id_admin INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-nombre_admin VARCHAR (50) NOT NULL,
-apellido_admin VARCHAR (50) NOT NULL,
-dui_admin VARCHAR (10) NOT NULL, 
-correo_admin VARCHAR (100) NOT NULL,
-telefono_admin VARCHAR (9) NOT NULL,
-nacimiento_admin DATE NOT NULL
+CREATE TABLE tb_empleado(
+id_empleado INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+nombre_empleado VARCHAR (50) NOT NULL,
+apellido_empleado VARCHAR (50) NOT NULL,
+dui_empleado VARCHAR (10) NOT NULL, 
+correo_empleado VARCHAR (100) NOT NULL,
+telefono_empleado VARCHAR (9) NOT NULL,
+nacimiento_empleado DATE NOT NULL,
+estado_empleado	bool
 ); 
 
 create table tb_usuarios(
@@ -37,8 +39,8 @@ id_usuario	int auto_increment key,
 nombre_usuario	varchar(50) unique not null,
 contrasenia		varchar(500) not null,
 foto_usuario	varchar(200),
-id_admin INT ,
-FOREIGN KEY (id_admin) REFERENCES tb_admin (id_admin)
+id_empleado INT ,
+FOREIGN KEY (id_empleado) REFERENCES tb_empleado (id_empleado)
 );
 
  CREATE TABLE tb_fotos (
@@ -73,16 +75,15 @@ FOREIGN KEY (id_cliente) REFERENCES tb_clientes (id_cliente)
 
 CREATE TABLE tb_citas(
 id_cita INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-fecha_cita date not null,
+fecha_cita datetime not null,
+estado_cita	bool,
 id_cliente INT ,
 FOREIGN KEY (id_cliente) REFERENCES tb_clientes (id_cliente), 
 id_servicio int, 
 FOREIGN KEY (id_servicio) REFERENCES tb_servicios (id_servicio)
 );
 
-
-
-CREATE TABLE tb_tratamientos (
+CREATE TABLE tb_nombres_tratamientos (
 id_tratamiento INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 tipo_de_tratamiento VARCHAR (75) NOT NULL,
 notas_adicionales VARCHAR (250),
@@ -95,17 +96,5 @@ id_comentario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 contenido_comentario VARCHAR (250) NOT NULL,
 id_cliente INT ,
 FOREIGN KEY (id_cliente) REFERENCES tb_clientes (id_cliente)
-);
-
-create table tb_sistemas(
-id_sistema int primary key auto_increment not null,
-id_cita INT,
-FOREIGN KEY (id_cita) REFERENCES tb_citas(id_cita),
-id_servicio int, 
-FOREIGN KEY (id_servicio) REFERENCES tb_servicios (id_servicio),
-id_tratamiento int,
-FOREIGN KEY (id_tratamiento) REFERENCES tb_tratamientos (id_tratamiento),
-id_usuario int,
-FOREIGN KEY (id_usuario) REFERENCES tb_usuarios (id_usuario)
 );
 
