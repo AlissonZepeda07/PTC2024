52923c851652c97f3e514a85a511f05baad308e8
Jhonnyy0 <136754303+Jhonnyy0@users.noreply.github.com>
2024-04-28 22:44:18 -0600

    Actualizado cruds, responsive y validaciones de la pantalla de pacientes

--- controlles/privada/validaciones.js
+++ controlles/privada/validaciones.js
@@ -10,10 +10,12 @@ function formatPhoneNumber(input) {
     // Validar que solo se permitan números y el formato XXXX-XXXX
     let regex = /^[0-9]{4}-?[0-9]{4}$/;
     if (!regex.test(phoneNumber)) {
-        input.setCustomValidity("El formato debe ser 0000-0000.");
+
+        // Mostrar mensaje de error
         input.classList.add("is-invalid");
     }
 
+    // En caso de pasar el test del formato
     else {
         input.setCustomValidity("");
         input.classList.remove("is-invalid");
@@ -38,40 +40,93 @@ function formatPhoneNumber(input) {
     }
 }
 
-//Codigo de validacion de campo email
+// Codigo de validacion de campo email
 function formatEmail(input) {
 
-    //Obtener el valor actual del campo de entrada
+    // Obtener el valor actual del campo de entrada
     let Email = input.value;
 
-    // Validar formato de correo electrónico ABC@
-    var emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+    // Validar formato de correo electrónico ABC@gmail.com
+    let emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
     if (!emailPattern.test(Email)) {
 
         // Mostrar mensaje de error
+        input.classList.add("is-invalid");
+    }
 
-        return false;
+    // En caso de pasar el test del formato
+    else {
+        input.setCustomValidity("");
+        input.classList.remove("is-invalid");
     }
 
     // Establecer el valor formateado en el campo de entrada
     input.value = Email;
 }
 
-//codigo de validacion de campo de contraseña
+// Codigo de validacion de campo de contraseña
 function formatPassword(input) {
 
+    // Obtener el valor actual del campo de entrada
     let Contraseña = input.value;
 
     // Validar longitud mínima de contraseña
     if (Contraseña.length < 8) {
 
         // Mostrar mensaje de error
+        input.classList.add("is-invalid");
+    }
 
-        return false;
+    // En caso de pasar el test del formato
+    else {
+        input.setCustomValidity("");
+        input.classList.remove("is-invalid");
     }
 
     // Establecer el valor formateado en el campo de entrada
     input.value = Contraseña;
+}
+
+//Codigo de validacion de campo de Dui
+function formatDui(input) {
+
+    //Obtener el valor actual del campo de entrada
+    let Dui = input.value;
+
+    // Eliminar cualquier guion existente
+    Dui = Dui.replace(/-/g, '');
+
+    // Establecer el formato de tipo Dui
+    let duiPattern = /^\d{8}-\d$/;
+    if (!duiPattern.test(Dui)) {
+
+        // Mostrar mensaje de error
+        input.classList.add("is-invalid");
+    }
+
+    // En caso de pasar el test del formato
+    else {
+        input.setCustomValidity("");
+        input.classList.remove("is-invalid");
+    }
+
+    // Agregar el guion después del cuarto dígito si no se ha agregado anteriormente
+    if (Dui.length >= 9 && Dui.charAt(8) !== '-') {
+        Dui = Dui.slice(0, 8) + '-' + Dui.slice(8);
+    }
+
+    // Establecer el valor formateado en el campo de entrada
+    input.value = Dui;
+
+    // Limitar la cantidad máxima de caracteres a 9
+    if (Dui.length >= 10) {
+        input.value = Dui.slice(0, 10);
+        input.setAttribute("maxlength", "10");
+    }
+
+    else {
+        input.removeAttribute("maxlength");
+    }
 }
 
 // Codigo de validacion campos vacios
